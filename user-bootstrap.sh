8679a8ace56d7454f10541b1403d301b7131fd15
Arjun Guha <arjun.guha@gmail.com>
2014-11-01 13:42:41 -0400

    Deps and submodules for frenetic/vzy2_dev

--- user-bootstrap.sh
+++ user-bootstrap.sh
@@ -4,4 +4,5 @@ set -x
 opam init -y
 opam config env
 echo 'eval `opam config env`' >> /home/vagrant/.profile
-opam install -y async quickcheck cmdliner ipaddr ulex cstruct ocamlgraph
+opam install -y async quickcheck cmdliner ipaddr ulex cstruct ocamlgraph \
+  cohttp base64 yojson
