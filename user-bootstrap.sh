6be0e34620332a52a6dd3ab4057ab9758c69df84
Arjun Guha <arjun@cs.umass.edu>
2014-11-12 16:37:23 -0500

    OMG IT COMPILES

--- user-bootstrap.sh
+++ user-bootstrap.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -x
+set -e
 
 opam init -y
 eval `opam config env`
 echo 'eval `opam config env`' >> /home/vagrant/.profile
-opam install -y async quickcheck cmdliner ipaddr ulex cstruct ocamlgraph \
-  base64 yojson oasis uri conduit
+opam pin add -y core 111.25.00
+opam pin add -y async 111.25.00
+opam install -y quickcheck cmdliner ipaddr ulex cstruct ocamlgraph \
+  base64 yojson oasis uri conduit cohttp
 
 (cd src/ocaml-cohttp; make; sudo make reinstall)
 (cd src/packet; ./configure --enable-quickcheck; make; make reinstall)
