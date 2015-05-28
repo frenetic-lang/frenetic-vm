78a1d33a42c47e4d749dbf62b3ee3c17b330901b
Craig Riecke <cr396@cornell.edu>
2015-05-28 10:04:26 -0400

    D'oh.  Reverted back too far.  Fast-forward and clean.

--- root-bootstrap.sh
+++ root-bootstrap.sh
@@ -6,6 +6,7 @@ apt-get update
 apt-get install -y \
   wireshark \
   m4 \
+  mininet \
   git \
   xterm \
   fortune \
@@ -19,13 +20,6 @@ apt-get install -y \
   python-pycurl \
   python-networkx \
   software-properties-common \
-
-# Install Mininet from source to get the latest version, > 2.2.1.  The API on the packaged version, 2.1, doesn't work
-# with the latest openvswitch.  https://github.com/frenetic-lang/frenetic-vm/issues/4
-cd /usr/local/src
-git clone git://github.com/mininet/mininet
-cd mininet
-util/install.sh -nfv
 
 add-apt-repository -y ppa:avsm/ppa
 apt-get update
--- user-bootstrap.sh
+++ user-bootstrap.sh
@@ -6,18 +6,12 @@ opam init -y
 eval `opam config env`
 echo 'eval `opam config env`' >> /home/vagrant/.profile
 
-opam install -y ulex cmdliner ocamlgraph quickcheck async yojson cohttp base64
+opam install -y ulex cmdliner ocamlgraph quickcheck async yojson cohttp base64 mparser
 
 cd src
-git clone https://github.com/frenetic-lang/ocaml-packet
-git clone https://github.com/frenetic-lang/ocaml-topology
-git clone https://github.com/frenetic-lang/ocaml-openflow
 git clone https://github.com/frenetic-lang/frenetic
 cd ..
 
-opam pin add packet src/ocaml-packet -n -k git
-opam pin add topology src/ocaml-topology -n -k git
-opam pin add openflow src/ocaml-openflow -n -k git
 opam pin add frenetic src/frenetic -n -k git
 
 opam install -y frenetic
